18fd13fc3c729c42e5bb7debd1cda87794f7436a
Gnana <gthoppae@gmail.com>
2017-10-11 08:12:36 +0200

    adding b64 and main script modifications

--- get-pdb-image.py
+++ get-pdb-image.py
@@ -1,10 +1,10 @@
 import urllib2
-input_file = open("data/porin.txt")
+input_file = open("data/hemoglobin-test.txt")
 for i in input_file.read().split('\n'):
     print "processing " + i
     link = "http://www.rcsb.org/pdb/images/%s_bio_r_500.jpg?bioNum=1" % i
     response = urllib2.urlopen(link)
     image = response.read()
     out_file = i + "-500.jpg"
-    f = open("data/porin/"+out_file,"wb")
+    f = open("data/images/test/hemoglobin/"+out_file,"wb")
     f.write(image)
